18df2b8fd699f92fc2383d61f1983b3aefb988a5
PrettyScript <JessicaDelcy@gmail.com>
2020-07-08 15:48:04 -0500

    added JavaFX file

---
+++ src/src/com/connect4/ConnectFour.java
@@ -0,0 +1,16 @@
+package com.connect4;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ConnectFour extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
